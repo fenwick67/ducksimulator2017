bdbb0f2bd79b12b38dbffbce8348b5a91a873d12
fenwick67 <smileman66@gmail.com>
2016-04-17 11:34:28 -0500

    add music

---
+++ Assets/GameAudio.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameAudio : MonoBehaviour {
+
+	private static GameAudio instance = null;
+	public static GameAudio Instance {
+	 	get { return instance; }
+ 	}
+
+	 void Awake() {
+		 if (instance != null && instance != this) {
+		     Destroy(this.gameObject);
+		     return;
+		 } else {
+		     instance = this;
+		 }
+
+	     DontDestroyOnLoad(this.gameObject);
+	 }
+
+
+}
