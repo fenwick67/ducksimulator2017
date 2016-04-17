3b574d7e8c213a5d3525b7d8b589d04c30f2461f
fenwick67 <smileman66@gmail.com>
2016-04-17 11:57:55 -0500

    add boilerplate and fix music, add .gitignore

--- Assets/GameAudio.cs
+++ Assets/GameAudio.cs
@@ -3,12 +3,20 @@ using System.Collections;
 
 public class GameAudio : MonoBehaviour {
 
+	public AudioClip PlayNewAudio = null;
+
 	private static GameAudio instance = null;
 	public static GameAudio Instance {
 	 	get { return instance; }
  	}
 
 	 void Awake() {
+
+		//TODO before killing myself change the audio if created with a <playnew> property
+		if (PlayNewAudio != null){
+			PlayNew (PlayNewAudio);
+		}
+
 		 if (instance != null && instance != this) {
 		     Destroy(this.gameObject);
 		     return;
@@ -18,6 +26,13 @@ public class GameAudio : MonoBehaviour {
 
 	     DontDestroyOnLoad(this.gameObject);
 	 }
+
+	public void PlayNew(AudioClip clip){
+		AudioSource src = GetComponent<AudioSource>();
+		src.Stop ();
+		src.clip = clip;
+		src.Play ();
+	}
 
 
 }
